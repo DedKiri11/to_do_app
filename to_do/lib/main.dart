c3a69ebdf5ef73539f8459b4ecf9f02225bc2b4b
Kirill Zezyukov <zezuykov@mail.ru>
2023-07-11 17:01:17 +0300

    Создание проекта
    Создание проекта в Android Studio

---
+++ to_do/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:to_doo/pages/home.dart';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.amberAccent,
+  ),
+  home: Home(),
+));
+
